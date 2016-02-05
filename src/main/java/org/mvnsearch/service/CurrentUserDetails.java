b21c4c8565ab1c8c8f9aa5e6a2bc64a5833e696e
linux_china <linux_china@hotmail.com>
2016-02-05 17:30:16 +0800

    current user details

---
+++ src/main/java/org/mvnsearch/service/CurrentUserDetails.java
@@ -0,0 +1,33 @@
+package org.mvnsearch.service;
+
+import org.mvnsearch.domain.User;
+import org.springframework.security.core.authority.AuthorityUtils;
+
+/**
+ * current user details
+ *
+ * @author linux_china
+ */
+public class CurrentUserDetails extends org.springframework.security.core.userdetails.User {
+    private User user;
+
+    public CurrentUserDetails(User user) {
+        super(user.getEmail(), user.getPasswordHash(), AuthorityUtils.createAuthorityList(user.getRole()));
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Long getId() {
+        return user.getId();
+    }
+
+    @Override
+    public String toString() {
+        return "CurrentUser{" +
+                "user=" + user +
+                "} " + super.toString();
+    }
+}

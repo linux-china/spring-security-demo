403b834efc7bcb2bd219bba343bfc4c3bd9954af
linux_china <linux_china@hotmail.com>
2016-02-05 17:26:47 +0800

    token authentication filter

---
+++ src/main/java/org/mvnsearch/config/TokenAuthenticationFilter.java
@@ -0,0 +1,43 @@
+package org.mvnsearch.config;
+
+import org.mvnsearch.domain.User;
+import org.mvnsearch.service.CurrentUser;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * token authentication filter
+ *
+ * @author linux_china
+ */
+public class TokenAuthenticationFilter extends OncePerRequestFilter {
+
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        //todo ignore static resources
+        String credentials = request.getHeader("Token");
+        if (credentials == null) {
+            Cookie cookie = WebUtils.getCookie(request, "token");
+            if (cookie != null) {
+                credentials = cookie.getValue();
+            }
+        }
+        if (credentials != null) {
+            User user = new User();
+            user.setEmail("admin@demo.com");
+            user.setId(1L);
+            user.setRole("buyer");
+            user.setPasswordHash(credentials);
+            CurrentUser currentUser = new CurrentUser(user);
+            SecurityContextHolder.getContext().setAuthentication(new TokenAuthentication(credentials, currentUser));
+        }
+        filterChain.doFilter(request, response);
+    }
+}

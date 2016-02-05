6213ea1c69611a3640654e5397e0d0db9e816e12
linux_china <linux_china@hotmail.com>
2016-02-05 17:33:02 +0800

    spring security demo application

---
+++ src/main/java/org/mvnsearch/SpringSecurityDemoApplication.java
@@ -0,0 +1,18 @@
+package org.mvnsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * spring security demo application
+ *
+ * @author linux_china
+ */
+@SpringBootApplication
+public class SpringSecurityDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityDemoApplication.class, args);
+    }
+
+}

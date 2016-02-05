a0a2deaf802837bf0bd3fe00cf01c66aca34fc57
linux_china <linux_china@hotmail.com>
2016-02-05 17:32:36 +0800

    domain user

---
+++ src/main/java/org/mvnsearch/domain/User.java
@@ -0,0 +1,45 @@
+package org.mvnsearch.domain;
+
+/**
+ * domain user
+ *
+ * @author linux_china
+ */
+public class User {
+    private Long id;
+    private String email;
+    private String passwordHash;
+    private String role;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}

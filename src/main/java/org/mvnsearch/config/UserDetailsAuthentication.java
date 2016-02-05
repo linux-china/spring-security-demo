cdc86c48a6e4663788bda122853626ac6f2895dc
linux_china <linux_china@hotmail.com>
2016-02-05 17:28:19 +0800

    User Details based authentication

---
+++ src/main/java/org/mvnsearch/config/UserDetailsAuthentication.java
@@ -0,0 +1,31 @@
+package org.mvnsearch.config;
+
+import org.mvnsearch.service.CurrentUser;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+
+/**
+ * User Details based authentication
+ *
+ * @author linux_china
+ */
+public class UserDetailsAuthentication extends AbstractAuthenticationToken {
+    private final Object principal;
+    private final Object credentials;
+
+    public UserDetailsAuthentication(Object credentials, CurrentUser currentUser) {
+        super(currentUser.getAuthorities());
+        this.principal = currentUser;
+        this.credentials = credentials;
+        setAuthenticated(true);
+    }
+
+    @Override
+    public Object getCredentials() {
+        return this.credentials;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return principal;
+    }
+}

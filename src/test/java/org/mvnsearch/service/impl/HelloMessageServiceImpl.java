3a3a0b3baa6a4bd37b46748760c7212d3587eb18
linux_china <linux_china@hotmail.com>
2016-02-09 15:59:20 +0800

    hello service implementation

---
+++ src/test/java/org/mvnsearch/service/impl/HelloMessageServiceImpl.java
@@ -0,0 +1,21 @@
+package org.mvnsearch.service.impl;
+
+import org.mvnsearch.service.HelloMessageService;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+/**
+ * hello service implementation
+ *
+ * @author linux_china
+ */
+@Service
+public class HelloMessageServiceImpl implements HelloMessageService {
+    @PreAuthorize("hasRole('ROLE_USER')")
+    public String getMessage() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return "Hello " + authentication;
+    }
+}

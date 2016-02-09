2695ac725e5901ec60a6a4477b43615f5509a518
linux_china <linux_china@hotmail.com>
2016-02-09 15:59:36 +0800

    spring security demo test

---
+++ src/test/java/org/mvnsearch/SpringSecurityDemoTest.java
@@ -0,0 +1,29 @@
+package org.mvnsearch;
+
+import org.junit.Test;
+import org.mvnsearch.service.HelloMessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ContextConfiguration;
+
+/**
+ * spring security demo test
+ *
+ * @author linux_china
+ */
+@ContextConfiguration(classes = SampleWebSecurityConfig.class)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@WithMockUser(username = "admin", roles = {"USER", "ADMIN"})
+public class SpringSecurityDemoTest extends SpringTest {
+    @Autowired
+    private HelloMessageService helloMessageService;
+
+    @Test
+    public void testSpike() {
+        String message = helloMessageService.getMessage();
+        System.out.println(message);
+    }
+
+
+}

a6a2496b0b58845ac8c9d2e182ef79b937e202e2
linux_china <linux_china@hotmail.com>
2016-02-10 11:35:25 +0800

    minor update

--- src/main/java/org/mvnsearch/config/WebSecurityConfig.java
+++ src/main/java/org/mvnsearch/config/WebSecurityConfig.java
@@ -4,12 +4,16 @@ import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
@@ -61,9 +65,20 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         web.ignoring().antMatchers("/", "/css/**", "/doLogin").and();
     }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+    }
+
     @Bean
     public Filter tokenAuthenticationFilter() {
         return new TokenAuthenticationFilter();
+    }
+
+
+    @Bean
+    public AuthenticationManager authenticationManager() throws Exception {
+        return authenticationManagerBean();
     }
 
 }

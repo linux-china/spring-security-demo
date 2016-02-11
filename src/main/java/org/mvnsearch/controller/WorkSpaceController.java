424678f229f456a644dcce1bedeccb8d32ff0b90
linux_china <linux_china@hotmail.com>
2016-02-11 15:45:18 +0800

    minor update

--- src/main/java/org/mvnsearch/controller/WorkSpaceController.java
+++ src/main/java/org/mvnsearch/controller/WorkSpaceController.java
@@ -1,9 +1,7 @@
 package org.mvnsearch.controller;
 
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 

4831e94ac8293a7b1aa37711fb7793c7245d4423
linux_china <linux_china@hotmail.com>
2016-02-11 15:44:34 +0800

    add password encoder

--- src/main/java/org/mvnsearch/controller/LoginController.java
+++ src/main/java/org/mvnsearch/controller/LoginController.java
@@ -1,5 +1,13 @@
 package org.mvnsearch.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.RememberMeServices;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,8 +22,13 @@ import javax.servlet.http.HttpServletResponse;
  * @author linux_china
  */
 @Controller
-
 public class LoginController {
+    @Autowired
+    private RememberMeServices rememberMeServices;
+    @Autowired
+    private UserDetailsService userDetailsService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @RequestMapping(path = "/login", method = RequestMethod.GET)
     public String login(HttpServletRequest request) {
@@ -25,10 +38,11 @@ public class LoginController {
     @RequestMapping(path = "/doLogin", method = RequestMethod.POST)
     public String doLogin(HttpServletRequest request, HttpServletResponse response) {
         String email = request.getParameter("email");
-        if ("admin@demo.com".equals(email)) {
-            Cookie token = new Cookie("token", "1234567");
-            token.setMaxAge(7 * 24 * 60 * 60);
-            response.addCookie(token);
+        String password = request.getParameter("password");
+        UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+        if (userDetails != null && "admin".equals(email) && passwordEncoder.matches(password, userDetails.getPassword())) {
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, userDetails.getPassword(), userDetails.getAuthorities());
+            rememberMeServices.loginSuccess(request, response, authentication);
             return "redirect:/home";
         }
         return "login";

1c5daa1eeb0319b0c9a345f6240726ca4b75a5bf
linux_china <linux_china@hotmail.com>
2016-02-15 07:57:45 +0800

    add @AuthenticationPrincipal

--- src/main/java/org/mvnsearch/controller/WorkSpaceController.java
+++ src/main/java/org/mvnsearch/controller/WorkSpaceController.java
@@ -1,7 +1,9 @@
 package org.mvnsearch.controller;
 
+import org.mvnsearch.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -18,7 +20,7 @@ public class WorkSpaceController {
     private ApplicationContext applicationContext;
 
     @RequestMapping("/home")
-    public String home(HttpServletRequest request) {
+    public String home(@AuthenticationPrincipal User user, HttpServletRequest request) {
         return "home";
     }
 }

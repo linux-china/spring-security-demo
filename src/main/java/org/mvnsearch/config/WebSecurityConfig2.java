915d3f2813ac58832cea08cc67d44ced63b4458d
linux_china <linux_china@hotmail.com>
2016-03-14 07:57:24 +0800

    web security common

---
+++ src/main/java/org/mvnsearch/config/WebSecurityConfig2.java
@@ -0,0 +1,35 @@
+package org.mvnsearch.config;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.rememberme.RememberMeAuthenticationFilter;
+
+import static com.google.common.base.Predicates.and;
+
+/**
+ * web security config
+ *
+ * @author linux_china
+ */
+@Configuration
+@EnableWebSecurity
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER - 1)
+class WebSecurityConfig2 extends WebSecurityConfigurerAdapter {
+    private String[] whiteUrls = new String[]{"/", "/webjars/**", "/wro4j/**", "/static/**", "/js/**", "/images/**", "**/favicon.ico", "/login", "/css/**", "/doLogin"};
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(whiteUrls).permitAll();
+    }
+
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(whiteUrls);
+    }
+}

dfc20b08269a9945c7d01e52e8e80e082d885feb
linux_china <linux_china@hotmail.com>
2016-04-24 15:10:19 +0800

    workspace controller test

---
+++ src/test/java/org/mvnsearch/controller/WorkSpaceControllerTest.java
@@ -0,0 +1,50 @@
+package org.mvnsearch.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mvnsearch.SpringSecurityDemoApplication;
+import org.mvnsearch.service.CurrentUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * workspace controller test
+ *
+ * @author linux_china
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringSecurityDemoApplication.class)
+@WebAppConfiguration
+public class WorkSpaceControllerTest {
+    private MockMvc mockMvc;
+    @Autowired
+    private WebApplicationContext context;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).apply(springSecurity()).build();
+    }
+
+    @Test
+    @WithUserDetails
+    public void testGood() throws Exception {
+        org.mvnsearch.domain.User user = new org.mvnsearch.domain.User();
+        user.setEmail("libing.chen@gmail.com");
+        user.setId(1L);
+        user.setAuthoritiesText("ROLE_USER");
+        mockMvc.perform(get("/home").with(user(new CurrentUserDetails(user))))
+                .andExpect(status().isOk());
+    }
+}

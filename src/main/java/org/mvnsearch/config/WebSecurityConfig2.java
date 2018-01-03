ce35a01b13829c900a4bbed6022b5efa7f60c95f
linux_china <linux_china@hotmail.com>
2018-01-03 11:47:25 +0800

    add white list

--- src/main/java/org/mvnsearch/config/WebSecurityConfig2.java
+++ src/main/java/org/mvnsearch/config/WebSecurityConfig2.java
@@ -1,16 +1,11 @@
 package org.mvnsearch.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.authentication.rememberme.RememberMeAuthenticationFilter;
-
-import static com.google.common.base.Predicates.and;
 
 /**
  * web security config
@@ -20,7 +15,8 @@ import static com.google.common.base.Predicates.and;
 @EnableWebSecurity
 @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER - 1)
 class WebSecurityConfig2 extends WebSecurityConfigurerAdapter {
-    private String[] whiteUrls = new String[]{"/", "/webjars/**", "/wro4j/**", "/static/**", "/js/**", "/images/**", "**/favicon.ico", "/login", "/css/**", "/doLogin"};
+    @Value("${security.ignored}")
+    private String[] whiteUrls;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -28,7 +24,4 @@ class WebSecurityConfig2 extends WebSecurityConfigurerAdapter {
                 .antMatchers(whiteUrls).permitAll();
     }
 
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers(whiteUrls);
-    }
 }

92ccc4e219834b65495ace5f12f3749687f97d55
Libing Chen <libing.chen@gmail.com>
2023-02-23 16:47:12 +0800

    chore: use EnableMethodSecurity

--- src/main/java/org/mvnsearch/SpringSecurityDemoApplication.java
+++ src/main/java/org/mvnsearch/SpringSecurityDemoApplication.java
@@ -2,7 +2,7 @@ package org.mvnsearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 /**
@@ -12,7 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
  */
 @SpringBootApplication
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+@EnableMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SpringSecurityDemoApplication {
 
     public static void main(String[] args) {

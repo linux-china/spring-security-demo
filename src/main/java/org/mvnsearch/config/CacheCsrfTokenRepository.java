3572d9d08d1463a6b4eb826cef144755061f704b
Libing Chen <libing.chen@gmail.com>
2023-12-31 17:26:10 +0800

    chore: updated to jakarta

--- src/main/java/org/mvnsearch/config/CacheCsrfTokenRepository.java
+++ src/main/java/org/mvnsearch/config/CacheCsrfTokenRepository.java
@@ -4,8 +4,8 @@ import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.csrf.DefaultCsrfToken;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;

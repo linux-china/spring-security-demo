9b19ca28534be41595155706c95878540eb61cea
Libing Chen <libing.chen@gmail.com>
2023-12-31 17:35:47 +0800

    chore: API adjusted

--- src/test/java/org/mvnsearch/SampleWebSecurityConfig.java
+++ src/test/java/org/mvnsearch/SampleWebSecurityConfig.java
@@ -4,11 +4,11 @@ import org.mvnsearch.service.HelloMessageService;
 import org.mvnsearch.service.impl.HelloMessageServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
  * sample web security config
@@ -17,21 +17,24 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  */
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SampleWebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Bean
-    public HelloMessageService messageService() {
-        return new HelloMessageServiceImpl();
-    }
+@EnableMethodSecurity()
+public class SampleWebSecurityConfig {
+  @Bean
+  public HelloMessageService messageService() {
+    return new HelloMessageServiceImpl();
+  }
 
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("admin").password("password").roles("USER", "ADMIN")
-                .and().withUser("jacky").password("password").roles("USER");
-    }
 
-    @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
+  @Bean
+  public InMemoryUserDetailsManager userDetailsService() {
+    UserDetails user = User.withDefaultPasswordEncoder()
+      .username("admin")
+      .password("password")
+      .roles("ADMIN")
+      .username("jacky")
+      .password("password")
+      .roles("USER")
+      .build();
+    return new InMemoryUserDetailsManager(user);
+  }
 }
